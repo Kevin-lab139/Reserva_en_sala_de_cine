00cf683fdce8327418a953d4cab48ee72629a6dd
JorgeCruel <jorgecruel119@hotmail.com>
2024-08-31 15:23:49 -0500

    funciones asientos y reservas

--- ProyectoPIO.py
+++ ProyectoPIO.py
@@ -25,6 +25,31 @@ class Cine:
             else:
                 print("Error: Por favor seleccione asientos válidos.")
 
+    def validar_asiento(self, asiento):
+        # Verificar si el asiento está dentro del rango válido
+        if not asiento.isdigit() or int(asiento) < 1 or int(asiento) > self.num_asientos:
+            return False
+        
+        # Verificar si el asiento ya está ocupado
+        for fila in self.SALA:
+            if asiento in fila:
+                return fila[fila.index(asiento)] != "X"
+        
+        return False
+
+    def realizar_reserva(self, asiento, usuario):
+        asientos_tupla = tuple(asiento)
+        self.RESERVAS[asientos_tupla] = {"usuario": usuario}
+
+        # Marcar los asientos como ocupados en la sala
+        for fila in self.SALA:
+            for i, num in enumerate(fila):
+                if num in asiento:
+                    fila[i] = "X"
+
+        print("Reserva realizada con éxito.")
+
+
 
 
 
