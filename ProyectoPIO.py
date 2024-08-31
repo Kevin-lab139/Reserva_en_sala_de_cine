ec6cb0767f555309ee488e8fb09d37bbf5a9b4cf
Kevin <kevidavidguerreroperea@gmail.com>
2024-08-31 15:28:48 -0500

    FuncionVerReservas

--- ProyectoPIO.py
+++ ProyectoPIO.py
@@ -48,7 +48,14 @@ class Cine:
                     fila[i] = "X"
 
         print("Reserva realizada con éxito.")
-
+    
+    def ver_mis_reservas(self):
+        print("Mis reservas:")
+        if len(self.RESERVAS) == 0:
+            print("No hay reservas")
+        else:
+            for asientos, datos in self.RESERVAS.items():
+                print(f"Asientos {asientos}: Usuario {datos['usuario']}")
 
 
 
